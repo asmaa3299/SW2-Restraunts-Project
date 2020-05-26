3d12eabb7e5020227c3e5b037c6ae8241f678953
MennaEmadZ <50415035+MennaEmadZ@users.noreply.github.com>
2020-05-26 02:10:16 +0200

    Add files via upload
    a new function is added called view registration in admin controller,  the admin can view all the registed users and other admin he can view there (name,email,phone number, job title) and i added 2 buttons that later can delete or edit the users info

---
+++ web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+//for redirect to homepage which is shown first 
+Route::get('/', function () {
+    return view('index');
+});
+
+//for login and registration 
+Auth::routes();
+
+//for redirect to admin/user homepage 
+Route::get('/home', 'HomeController@index');
+Route::get('/Reservation/viewReservation' , 'ReservationController@viewReservation');
+
+
+Route::get('/Reservation/makeReservation' , 'ReservationController@makeReservation');
+
+Route::post('/Reservation' , 'ReservationController@store');
+
+Route::get('/registration/getInfo','adminController@getData')->name('UserInfo');
+
+Route::delete('/Reservation/delete/{id}' , 'ReservationController@destroy');
